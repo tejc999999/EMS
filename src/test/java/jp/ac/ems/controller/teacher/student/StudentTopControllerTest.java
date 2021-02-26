165fa105f1966840ec81be3e226474f1aaec3ea9
tejc999999 <tejc999999@gmail.com>
2021-02-27 00:24:49 +0900

    学生トップページテスト作成完了.

--- src/test/java/jp/ac/ems/controller/teacher/student/StudentTopControllerTest.java
+++ src/test/java/jp/ac/ems/controller/teacher/student/StudentTopControllerTest.java
@@ -64,21 +64,6 @@ public class StudentTopControllerTest {
 	
 	// 本日日付
 	private static final Date NOW_DATE = new Date();
-	
-	
-    // 今週の月曜日と金曜日
-
-    // 前週の月曜日と金曜日
-
-	// 先週日付
-//	Calendar cal = ;
-    		
-    		
-//    		.add(Calendar.DAY_OF_WEEK, -7);
-	
-	// 先々週日付
-	
-	
 
 	// 学生１人目
 	private static final String INSERT_STUDENT1_ID = "unitstudent1";
@@ -558,6 +543,47 @@ public class StudentTopControllerTest {
     				INSERT_DATE_NOW,
     				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_UPDATE_USER_ID).build();    
     
+    // 前週：正解1:1(大分類), 1(中分類), 1(小分類)
+    private static final Long INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_ID_1WEEKAGO = Long.valueOf("39");
+    private static final Operation INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_1WEEKAGO = Operations.insertInto(
+    		"t_student_question_history").columns("id", "correct_flg", "question_id", "update_date", "user_id").values(
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_ID_1WEEKAGO,
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_CORRECT_FLG,
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_QUESTION_ID,
+    				INSERT_DATE_1WEEKAGO,
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_UPDATE_USER_ID).build();
+    
+    // 前週不正解1:1(大分類), 1(中分類), 1(小分類)
+    private static final Long INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_ID_1WEEKAGO = Long.valueOf("40");
+    private static final Operation INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_1WEEKAGO = Operations.insertInto(
+    		"t_student_question_history").columns("id", "correct_flg", "question_id", "update_date", "user_id").values(
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_ID_1WEEKAGO,
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_CORRECT_FLG,
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_QUESTION_ID,
+    				INSERT_DATE_1WEEKAGO,
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_UPDATE_USER_ID).build();    
+    
+    // 前々週：正解1:1(大分類), 1(中分類), 1(小分類)
+    private static final Long INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_ID_2WEEKAGO = Long.valueOf("41");
+    private static final Operation INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_2WEEKAGO = Operations.insertInto(
+    		"t_student_question_history").columns("id", "correct_flg", "question_id", "update_date", "user_id").values(
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_ID_2WEEKAGO,
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_CORRECT_FLG,
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_QUESTION_ID,
+    				INSERT_DATE_2WEEKAGO,
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_UPDATE_USER_ID).build();
+    
+    // 前々週不正解1:1(大分類), 1(中分類), 1(小分類)
+    private static final Long INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_ID_2WEEKAGO = Long.valueOf("42");
+    private static final Operation INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_2WEEKAGO = Operations.insertInto(
+    		"t_student_question_history").columns("id", "correct_flg", "question_id", "update_date", "user_id").values(
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_ID_2WEEKAGO,
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_CORRECT_FLG,
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_QUESTION_ID,
+    				INSERT_DATE_2WEEKAGO,
+    				INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_UPDATE_USER_ID).build();    
+
+    
     // 全学生削除
     private static final Operation DELETE_ALL_STUDENT_DATA = Operations.sql(
     		"DELETE FROM t_user");
@@ -665,7 +691,6 @@ public class StudentTopControllerTest {
    	 * 他の学生が問題を解いた状態で、問題を解いていない学生がトップページを表示する.
      * @throws Exception MockMVC失敗時例外
      */
-	@SuppressWarnings("unchecked")
 	@Test
     @WithUserDetails(value=INSERT_STUDENT1_ID, userDetailsServiceBeanName="UserDetailService")
     public void 他の学生が問題を解いた状態で問題を解いていない学生がトップページを表示する_正常() throws Exception {
@@ -715,27 +740,319 @@ public class StudentTopControllerTest {
     	assertThat(form.getPrevWeekStrategyCorrectRate()).isEqualTo("0.0");
     }
 
-	
+
+	/**
+	 * 今週のみ問題を解いた状態で学生トップページを表示する.
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+    @WithUserDetails(value=INSERT_STUDENT1_ID, userDetailsServiceBeanName="UserDetailService")
+    public void 今週のみ問題を解いた状態で学生トップページを表示する_正常() throws Exception {
+        // DB状態
+        Destination dest = new DataSourceDestination(dataSource);
+        // 合計：正解率：2/4, 回答数：4
+        // テクノロジ：正解率：1/2, 回答数:2
+        // マネジメント：正解率：1/1, 回答数：1
+        // ストラテジ：正解率：0/1, 回答数：1
+        Operation ops = Operations.sequenceOf(
+        		INSERT_STUDENT1_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_NOW,
+        		INSERT_STUDENT1_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_NOW,
+        		INSERT_STUDENT1_QUESTION_HISTORY_2_5_14_CORRECT_DATA1_NOW,
+        		INSERT_STUDENT1_QUESTION_HISTORY_3_9_20_INCORRECT_DATA1_NOW,
+        		
+        		INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_NOW,
+        		INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_NOW
+        		);
+        
+        DbSetup dbSetup = new DbSetup(dest, ops);
+        dbSetup.launch();
+		
+    	MvcResult result = mockMvc.perform(get("/student/top"))
+    		.andExpect(status().is2xxSuccessful()).andExpect(view().name(
+                "student/top")).andReturn();
+
+    	TopForm form = (TopForm) result.getModelAndView().getModel().get("topForm");
+
+    	// 累計の情報
+    	assertThat(form.getTotalAnswerCnt()).isEqualTo("4");
+    	assertThat(form.getTotalCorrectRate()).isEqualTo("50.0");
+    	assertThat(form.getTotalTechnorogyAnswerCnt()).isEqualTo("2");
+    	assertThat(form.getTotalTechnorogyCorrectRate()).isEqualTo("50.0");
+    	assertThat(form.getTotalManagementAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getTotalManagementCorrectRate()).isEqualTo("100.0");
+    	assertThat(form.getTotalStrategyAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getTotalStrategyCorrectRate()).isEqualTo("0.0");
+
+    	// 今週の情報
+    	assertThat(form.getWeekAnswerCnt()).isEqualTo("4");
+    	assertThat(form.getWeekCorrectRate()).isEqualTo("50.0");
+    	assertThat(form.getWeekTechnorogyAnswerCnt()).isEqualTo("2");
+    	assertThat(form.getWeekTechnorogyCorrectRate()).isEqualTo("50.0");
+    	assertThat(form.getWeekManagementAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getWeekManagementCorrectRate()).isEqualTo("100.0");
+    	assertThat(form.getWeekStrategyAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getWeekStrategyCorrectRate()).isEqualTo("0.0");
+    	
+    	// 前週の情報
+    	assertThat(form.getPrevWeekAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getPrevWeekCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getPrevWeekTechnorogyAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getPrevWeekTechnorogyCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getPrevWeekManagementAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getPrevWeekManagementCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getPrevWeekStrategyAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getPrevWeekStrategyCorrectRate()).isEqualTo("0.0");
+    }
+
+	/**
+	 * 前週のみ問題を解いた状態で学生トップページを表示する.
+	 * @throws Exception
+	 */
+	@Test
+    @WithUserDetails(value=INSERT_STUDENT1_ID, userDetailsServiceBeanName="UserDetailService")
+    public void 前週のみ問題を解いた状態で学生トップページを表示する_正常() throws Exception {
+        // DB状態
+        Destination dest = new DataSourceDestination(dataSource);
+        // 合計：正解率：3/5, 回答数：5
+        // テクノロジ：正解率：2/3, 回答数:3
+        // マネジメント：正解率：0/1, 回答数：1
+        // ストラテジ：正解率：1/1, 回答数：1
+        Operation ops = Operations.sequenceOf(
+        		INSERT_STUDENT1_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_1WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_1_1_1_CORRECT_DATA2_1WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_1WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_2_5_14_INCORRECT_DATA1_1WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_3_9_20_CORRECT_DATA1_1WEEKAGO,
+        		
+        		INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_1WEEKAGO,
+        		INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_1WEEKAGO
+        		);
+        
+        DbSetup dbSetup = new DbSetup(dest, ops);
+        dbSetup.launch();
+		
+    	MvcResult result = mockMvc.perform(get("/student/top"))
+    		.andExpect(status().is2xxSuccessful()).andExpect(view().name(
+                "student/top")).andReturn();
+
+    	TopForm form = (TopForm) result.getModelAndView().getModel().get("topForm");
+
+    	// 累計の情報
+    	assertThat(form.getTotalAnswerCnt()).isEqualTo("5");
+    	assertThat(form.getTotalCorrectRate()).isEqualTo("60.0");
+    	assertThat(form.getTotalTechnorogyAnswerCnt()).isEqualTo("3");
+    	assertThat(form.getTotalTechnorogyCorrectRate()).isEqualTo("66.7");
+    	assertThat(form.getTotalManagementAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getTotalManagementCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getTotalStrategyAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getTotalStrategyCorrectRate()).isEqualTo("100.0");
+
+    	// 今週の情報
+    	assertThat(form.getWeekAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getWeekCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getWeekTechnorogyAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getWeekTechnorogyCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getWeekManagementAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getWeekManagementCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getWeekStrategyAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getWeekStrategyCorrectRate()).isEqualTo("0.0");
+    	
+    	// 前週の情報
+    	assertThat(form.getPrevWeekAnswerCnt()).isEqualTo("5");
+    	assertThat(form.getPrevWeekCorrectRate()).isEqualTo("60.0");
+    	assertThat(form.getPrevWeekTechnorogyAnswerCnt()).isEqualTo("3");
+    	assertThat(form.getPrevWeekTechnorogyCorrectRate()).isEqualTo("66.7");
+    	assertThat(form.getPrevWeekManagementAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getPrevWeekManagementCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getPrevWeekStrategyAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getPrevWeekStrategyCorrectRate()).isEqualTo("100.0");
+    }
+
     /**
-     * 今週のみ問題を解いた状態で学生トップページを表示する（回答数、正解率確認）.＋テクノロジ、マネジメント、ストラテジあり（回答数、正解率確認）
-     * 前週のみ問題を解いた状態で学生トップページを表示する（回答数、正解率確認）.＋テクノロジ、マネジメント、ストラテジあり（回答数、正解率確認）
-     * 今週と前週は問題を解かず、それ以前にのみ問題を解いた状態でトップページを表示する（回答数、正解率確認）.＋テクノロジ、マネジメント、ストラテジあり（回答数、正解率確認）
-     * 今週と前週とそれ以前の問題を解いた状態、かつ分野は一部解答一部未回答の状態で学生トップページを表示する.
-     * 今週の年月日、前週の年月日を正しく取得した状態で学生トップページが表示される.
-     */
+	 * 今週と前週は問題を解かず、それ以前にのみ問題を解いた状態でトップページを表示する.
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+    @WithUserDetails(value=INSERT_STUDENT1_ID, userDetailsServiceBeanName="UserDetailService")
+    public void 今週と前週は問題を解かずそれ以前にのみ問題を解いた状態でトップページを表示する_正常() throws Exception {
+        // DB状態
+        Destination dest = new DataSourceDestination(dataSource);
+        // 合計：正解率：3/5, 回答数：5
+        // テクノロジ：正解率：2/3, 回答数:3
+        // マネジメント：正解率：0/1, 回答数：1
+        // ストラテジ：正解率：1/1, 回答数：1
+        Operation ops = Operations.sequenceOf(
+        		INSERT_STUDENT1_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_2WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_1_1_1_CORRECT_DATA2_2WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_2WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_2_5_14_INCORRECT_DATA1_2WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_3_9_20_CORRECT_DATA1_2WEEKAGO,
+        		
+        		INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_2WEEKAGO,
+        		INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_2WEEKAGO
+        		);
+        
+        DbSetup dbSetup = new DbSetup(dest, ops);
+        dbSetup.launch();
+		
+    	MvcResult result = mockMvc.perform(get("/student/top"))
+    		.andExpect(status().is2xxSuccessful()).andExpect(view().name(
+                "student/top")).andReturn();
+
+    	TopForm form = (TopForm) result.getModelAndView().getModel().get("topForm");
+
+    	// 累計の情報
+    	assertThat(form.getTotalAnswerCnt()).isEqualTo("5");
+    	assertThat(form.getTotalCorrectRate()).isEqualTo("60.0");
+    	assertThat(form.getTotalTechnorogyAnswerCnt()).isEqualTo("3");
+    	assertThat(form.getTotalTechnorogyCorrectRate()).isEqualTo("66.7");
+    	assertThat(form.getTotalManagementAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getTotalManagementCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getTotalStrategyAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getTotalStrategyCorrectRate()).isEqualTo("100.0");
+
+    	// 今週の情報
+    	assertThat(form.getWeekAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getWeekCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getWeekTechnorogyAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getWeekTechnorogyCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getWeekManagementAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getWeekManagementCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getWeekStrategyAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getWeekStrategyCorrectRate()).isEqualTo("0.0");
+    	
+    	// 前週の情報
+    	assertThat(form.getPrevWeekAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getPrevWeekCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getPrevWeekTechnorogyAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getPrevWeekTechnorogyCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getPrevWeekManagementAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getPrevWeekManagementCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getPrevWeekStrategyAnswerCnt()).isEqualTo("0");
+    	assertThat(form.getPrevWeekStrategyCorrectRate()).isEqualTo("0.0");
+    }
+
     /**
-     * 【回答履歴】
-     * ３８問
-     * ・今週の大分類１、正解と不正解（２つずつ）
-     * ・今週の大分類２、正解と不正解（２つずつ）
-     * ・今週の大分類３、正解と不正解（２つずつ）
-     * ・前週の大分類１、正解と不正解（２つずつ）
-     * ・前週の大分類２、正解と不正解（２つずつ）
-     * ・前週の大分類３、正解と不正解（２つずつ）
-     * ・前週より前の大分類１、正解と不正解（２つずつ）
-     * ・前週より前の大分類２、正解と不正解（２つずつ）
-     * ・前週より前の大分類３、正解と不正解（２つずつ）
-     * ・他の学生の今週の大分類１、正解と不正解
-     */
-    
+	 * 今週と前週とそれ以前の問題を解いた状態、かつ分野は一部解答一部未回答の状態で学生トップページを表示する.
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+    @WithUserDetails(value=INSERT_STUDENT1_ID, userDetailsServiceBeanName="UserDetailService")
+    public void 今週と前週とそれ以前の問題を解いた状態で学生トップページを表示する_正常() throws Exception {
+        // DB状態
+        Destination dest = new DataSourceDestination(dataSource);
+        // 【累計】
+        // 合計：正解率：9/15, 回答数：15
+        // テクノロジ：正解率：3/5, 回答数:5
+        // マネジメント：正解率：2/5, 回答数：5
+        // ストラテジ：正解率：4/7, 回答数：7
+        // 【今週】
+        // 合計：正解率：3/5:回答数：5
+        // テクノロジ：正解率：1/1, 回答数:1
+        // マネジメント：正解率：0/1, 回答数：1
+        // ストラテジ：正解率：2/3, 回答数：3
+        // 【前週】
+        // 合計：正解率：3/5:回答数：5
+        // テクノロジ：正解率：0/1, 回答数:1
+        // マネジメント：正解率：2/3, 回答数：3
+        // ストラテジ：正解率：1/1, 回答数：1
+        // 【それ以前】
+        // 合計：
+        // テクノロジ：正解率：2/3, 回答数:3
+        // マネジメント：正解率：0/1, 回答数：1
+        // ストラテジ：正解率：1/1, 回答数：1
+        Operation ops = Operations.sequenceOf(
+        		INSERT_STUDENT1_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_NOW,
+        		INSERT_STUDENT1_QUESTION_HISTORY_2_5_14_INCORRECT_DATA1_NOW,
+        		INSERT_STUDENT1_QUESTION_HISTORY_3_9_20_CORRECT_DATA1_NOW,
+        		INSERT_STUDENT1_QUESTION_HISTORY_3_9_20_CORRECT_DATA2_NOW,
+        		INSERT_STUDENT1_QUESTION_HISTORY_3_9_20_INCORRECT_DATA1_NOW,
+
+        		INSERT_STUDENT1_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_1WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_2_5_14_CORRECT_DATA1_1WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_2_5_14_CORRECT_DATA2_1WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_2_5_14_INCORRECT_DATA1_1WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_3_9_20_CORRECT_DATA1_1WEEKAGO,
+
+        		INSERT_STUDENT1_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_2WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_1_1_1_CORRECT_DATA2_2WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_2WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_2_5_14_INCORRECT_DATA1_2WEEKAGO,
+        		INSERT_STUDENT1_QUESTION_HISTORY_3_9_20_CORRECT_DATA1_2WEEKAGO,
+        		
+        		INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_CORRECT_DATA1_2WEEKAGO,
+        		INSERT_STUDENT2_QUESTION_HISTORY_1_1_1_INCORRECT_DATA1_2WEEKAGO
+        		);
+        
+        DbSetup dbSetup = new DbSetup(dest, ops);
+        dbSetup.launch();
+		
+    	MvcResult result = mockMvc.perform(get("/student/top"))
+    		.andExpect(status().is2xxSuccessful()).andExpect(view().name(
+                "student/top")).andReturn();
+
+    	TopForm form = (TopForm) result.getModelAndView().getModel().get("topForm");
+
+    	// 累計の情報
+    	assertThat(form.getTotalAnswerCnt()).isEqualTo("15");
+    	assertThat(form.getTotalCorrectRate()).isEqualTo("60.0");
+    	assertThat(form.getTotalTechnorogyAnswerCnt()).isEqualTo("5");
+    	assertThat(form.getTotalTechnorogyCorrectRate()).isEqualTo("60.0");
+    	assertThat(form.getTotalManagementAnswerCnt()).isEqualTo("5");
+    	assertThat(form.getTotalManagementCorrectRate()).isEqualTo("40.0");
+    	assertThat(form.getTotalStrategyAnswerCnt()).isEqualTo("5");
+    	assertThat(form.getTotalStrategyCorrectRate()).isEqualTo("80.0");
+
+    	// 今週の情報
+    	assertThat(form.getWeekAnswerCnt()).isEqualTo("5");
+    	assertThat(form.getWeekCorrectRate()).isEqualTo("60.0");
+    	assertThat(form.getWeekTechnorogyAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getWeekTechnorogyCorrectRate()).isEqualTo("100.0");
+    	assertThat(form.getWeekManagementAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getWeekManagementCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getWeekStrategyAnswerCnt()).isEqualTo("3");
+    	assertThat(form.getWeekStrategyCorrectRate()).isEqualTo("66.7");
+    	
+    	// 前週の情報
+    	assertThat(form.getPrevWeekAnswerCnt()).isEqualTo("5");
+    	assertThat(form.getPrevWeekCorrectRate()).isEqualTo("60.0");
+    	assertThat(form.getPrevWeekTechnorogyAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getPrevWeekTechnorogyCorrectRate()).isEqualTo("0.0");
+    	assertThat(form.getPrevWeekManagementAnswerCnt()).isEqualTo("3");
+    	assertThat(form.getPrevWeekManagementCorrectRate()).isEqualTo("66.7");
+    	assertThat(form.getPrevWeekStrategyAnswerCnt()).isEqualTo("1");
+    	assertThat(form.getPrevWeekStrategyCorrectRate()).isEqualTo("100.0");
+    }
+
+    /**
+	 * 今週の年月日、前週の年月日を正しく取得した状態で学生トップページが表示される.
+	 * @throws Exception
+	 */
+	@Test
+    @WithUserDetails(value=INSERT_STUDENT1_ID, userDetailsServiceBeanName="UserDetailService")
+    public void 今週の年月日と前週の年月日を正しく取得した状態で学生トップページが表示される_正常() throws Exception {
+
+		
+    	MvcResult result = mockMvc.perform(get("/student/top"))
+    		.andExpect(status().is2xxSuccessful()).andExpect(view().name(
+                "student/top")).andReturn();
+    	
+    	TopForm form = (TopForm) result.getModelAndView().getModel().get("topForm");
+
+        // 現在日付
+        LocalDate now = LocalDate.now();
+        // 今週の月曜日と金曜日
+        LocalDate weekMonday = now.with(DayOfWeek.MONDAY);
+        LocalDate weekSunday = now.with(DayOfWeek.SUNDAY);
+        // 前週の月曜日と金曜日
+        LocalDate prevWeekMonday = weekMonday.minusDays(7);
+        LocalDate prevWeekSunday = weekSunday.minusDays(7);
+
+    	// 累計の情報
+    	assertThat(form.getWeekPeriod()).isEqualTo(weekMonday + " ~ " + weekSunday);
+    	assertThat(form.getPrevWeekPeriod()).isEqualTo(prevWeekMonday + " ~ " + prevWeekSunday);
+    }
 }
